711ed9b2fc95400588484e883be7d2f395e19070
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2022-11-08 12:14:49 +0000

    Acceleration Part 2

--- Assets/Script/LateUpdateMove.cs
+++ Assets/Script/LateUpdateMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LateUpdateMove : MonoBehaviour
 {
     public float speed = 0.5f;
+
     void LateUpdate()
     {
         this.transform.Translate(0, 0, Time.deltaTime * speed);
