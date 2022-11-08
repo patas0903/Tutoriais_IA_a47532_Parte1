a16f78acb96c970333fcf54b04b243572be0ecb9
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2022-11-08 14:46:47 +0000

    Calculating Trajectories Part 3

--- Assets/Scripts/AIShell.cs
+++ Assets/Scripts/AIShell.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AIShell : MonoBehaviour
 {
     public GameObject explosion;
+    Rigidbody rb;
     
 
     void OnCollisionEnter(Collision col)
@@ -19,12 +20,12 @@ public class AIShell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.forward = rb.velocity;
     }
 }
--- Assets/Scripts/FireShell.cs
+++ Assets/Scripts/FireShell.cs
@@ -10,20 +10,23 @@ public class FireShell: MonoBehaviour
     public GameObject enemy;
     public Transform turretBase;
     float speed = 15;
-    float rotSpeed = 2;
+    float rotSpeed = 5;
+    float moveSpeed = 1;
     // Start is called before the first frame update
     void CreateBullet()
     {
-        Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+        GameObject shell = Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+        shell.GetComponent<Rigidbody>().velocity = speed * turretBase.forward;
     }
 
-    void RotateTurret ()
+    float? RotateTurret()
     {
-        float? angle = CalculateAngle(true);
+        float? angle = CalculateAngle(false);
         if(angle != null)
         {
             turretBase.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
         }
+        return angle;
     }
 
     float? CalculateAngle(bool low)
@@ -31,7 +34,7 @@ public class FireShell: MonoBehaviour
         Vector3 targetDir = enemy.transform.position - this.transform.position;
         float y = targetDir.y;
         targetDir.y = 0f;
-        float x = targetDir.magnitude;
+        float x = targetDir.magnitude - 1;
         float gravity = 9.8f;
         float sSqr = speed * speed;
         float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
@@ -58,10 +61,14 @@ public class FireShell: MonoBehaviour
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
-        RotateTurret();
-        if (Input.GetKeyDown(KeyCode.Space))
+        float? angle = RotateTurret();
+        if (angle != null)
         {
           CreateBullet();
+        }
+        else
+        {
+            this.transform.Translate(0, 0, Time.deltaTime * moveSpeed);
         }
     }
 }
