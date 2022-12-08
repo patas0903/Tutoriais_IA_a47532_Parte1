526a8d4e6c05ee63a0089aa3c908cf5df431f990
Patrícia Guerreiro <a47532@alunos.ipb.pt>
2022-12-08 19:10:30 +0000

    Tutorial 4 - 1. Vectors Part 1

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -13,7 +13,11 @@ public class AIControl : MonoBehaviour {
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
         anim.SetTrigger("isWalking");
+        float sm = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
     }
 
     // Update is called once per frame
